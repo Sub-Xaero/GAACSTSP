87877f307e44c6135692e4013161f611aebe9b0b
Dylan Clarke <dylcla@me.com>
2018-03-29 21:00:43 +0100

    GA - Fitness( candidate )
    returns negative number as we need to minimine fitness rather than maximise. Lazy solution, can be later rectified

--- ga/fitness.go
+++ ga/fitness.go
@@ -11,3 +11,16 @@ func EuclideanDistance(c1, c2 City) float64 {
 	return sqSum
 }
 
+func (genA *GeneticAlgorithm) Fitness(gene Genome, cities map[string]City) float64 {
+	genomeSequence := gene.Copy().Sequence
+
+	totalDistance := 0.0
+	for i := 0; i < len(genomeSequence)-2; i++ {
+		city1, city2 := genomeSequence[i], genomeSequence[i+1]
+		if city2 == "-1" {
+			city2 = genomeSequence[0]
+		}
+		totalDistance += EuclideanDistance(cities[city1], cities[city2])
+	}
+	return 0.0 - totalDistance
+}

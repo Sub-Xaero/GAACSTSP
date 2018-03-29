e36e373f4f4df20b345519f066255e68da840340
Dylan Clarke <dylcla@me.com>
2018-03-29 21:01:58 +0100

    Bitstrings - Generate valid permutation sequence

--- ga/bitstrings.go
+++ ga/bitstrings.go
@@ -6,6 +6,25 @@ import (
 
 type Bitstring []string
 
+// GenerateCandidate returns an encoded string as set by calls SetGenerateBitString. Defaults to binary strings
+func (genA *GeneticAlgorithm) GenerateCandidate(length int, cities map[string]City) (Bitstring, error) {
+	keys := make([]string, 0)
+
+	for key := range cities {
+		keys = append(keys, key)
+	}
+
+	sequence := make(Bitstring, 0)
+	for i := 0; i < length; i++ {
+		choice := genA.RandomEngine.Int() % len(keys)
+		sequence = append(sequence, keys[choice])
+		keys = append(keys[:choice], keys[choice+1:]...)
+	}
+
+	sequence = append(sequence, "-1")
+	return sequence, nil
+}
+
 // SetGenerateBitString sets the function that generates the Bitstring candidatePool
 func (b Bitstring) String() string {
 	output := ""

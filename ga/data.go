6a5c87eb225aa30da5aed8156acc3d823435fbb2
Dylan Clarke <dylcla@me.com>
2018-04-01 22:57:21 +0100

    Data loader use regex for better section detection, extra error conditions

--- ga/data.go
+++ ga/data.go
@@ -18,28 +18,47 @@ func LoadTSPLib(filename string) map[string]City {
 	Check(err)
 	defer file.Close()
 
-	re := regexp.MustCompile(`\s+`)
-
 	scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() {
-		line := scanner.Text()
+	// Regex-ers
+	fileType := regexp.MustCompile(`^\s*TYPE\s*:`)
+	fixedEdgesSection := regexp.MustCompile(`^\s*FIXED_EDGES_SECTION`)
+	edgeWeightType := regexp.MustCompile(`^\s*EDGE_WEIGHT_TYPE\s*:`)
+	nodeCoordSection := regexp.MustCompile(`^\s*NODE_COORD_SECTION`)
 
-		if strings.Contains(line, "FIXED_EDGES_SECTION") {
+	// Read file header
+	for scanner.Scan() {
+		Check(scanner.Err())
+
+		line := scanner.Text()
+		Check(scanner.Err())
+
+		if fileType.MatchString(line) && !strings.Contains(line, "TSP") {
+			panic("Optimal tour file being loaded as input file")
+		}
+
+		if fixedEdgesSection.MatchString(line) {
 			panic("Program does not support fixed edges")
 		}
 
-		if strings.Contains(line, "EDGE_WEIGHT_TYPE") && !strings.Contains(line, "EUC_2D") {
+		if edgeWeightType.MatchString(line) && !strings.Contains(line, "EUC_2D") {
 			panic("Data file does not contain co-ordinates compatible with EUC_2D fitness function")
 		}
 
-		if strings.Compare(line, "NODE_COORD_SECTION") == 0 {
+		if nodeCoordSection.MatchString(line) {
 			break
 		}
 	}
 	count := 0
+
+	// Read file content
+	re := regexp.MustCompile(`\s+`)
 	for scanner.Scan() {
+		Check(scanner.Err())
+
 		line := scanner.Text()
+		Check(scanner.Err())
+
 		line = re.ReplaceAllString(line, " ")
 		line = strings.Trim(line, " ")
 
@@ -56,7 +75,6 @@ func LoadTSPLib(filename string) map[string]City {
 		y, convertErr := strconv.ParseFloat(data[2], 64)
 		Check(convertErr)
 
-		Check(scanner.Err())
 		cities[strconv.Itoa(count)] = City{x, y}
 	}
 

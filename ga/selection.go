6932e3d329b5f422c26ce0a9bc45617efca1ad11
Dylan Clarke <dylcla@me.com>
2018-04-04 14:29:06 +0100

    Selection - ACO Filter method

--- ga/selection.go
+++ ga/selection.go
@@ -56,6 +56,44 @@ func (genA *GeneticAlgorithm) RouletteSelection(candidatePool Population, cities
 	return offspring
 }
 
+// Permutation based problem, requires this function to filter down the candidate pool so that the returned population
+// contains only elements whose next chromosome to be picked is valid. e.g. with a a partial solution
+// of [ 0 1 ], only return elements whose N position such that elem = [ x x N x ... ], does not contain either 0 or 1
+func (genA *GeneticAlgorithm) ACOFilter(genePartial Bitstring, candidatePool Population, cities map[string]City) Population {
+	candidatePoolCopy := make(Population, len(candidatePool))
+	copy(candidatePoolCopy, candidatePool)
+	filteredOffspring := make(Population, 0)
+
+	numAlreadyVisited := len(genePartial)
+
+	// Loop over candidate pool
+	for i := 0; i < len(candidatePoolCopy); i++ {
+
+		// Only filter on elements in positions > those already picked as visited
+		// Slice containing the next possible element to be picked
+		remainingSlice := candidatePoolCopy[i].Sequence[numAlreadyVisited : numAlreadyVisited+1]
+
+		// Flag, if set to false in the below detection loop, elem is to be filtered out
+		allowed := true
+
+	toOuterLoop:
+		for _, remainingCity := range remainingSlice {
+			for _, visitedCity := range genePartial {
+				// Permutation based problem, so remove elements that would duplicate tours of a city if picked
+				if visitedCity == remainingCity {
+					allowed = false
+					break toOuterLoop
+				}
+			}
+		}
+		if allowed {
+			filteredOffspring = append(filteredOffspring, candidatePoolCopy[i])
+		}
+	}
+
+	return filteredOffspring
+}
+
 // SetSelectionFunc changes the selection function to the function specified
 func (genA *GeneticAlgorithm) Selection(candidatePool Population, cities map[string]City) Population {
 	return genA.TournamentSelection(candidatePool, cities)
